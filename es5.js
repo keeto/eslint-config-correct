3b5d7602fcd00d786693f09af456cb84c34dc0f0
Mark Obcena <markeeto@gmail.com>
2018-06-24 17:26:29 +0200

    Update imports.

--- es5.js
+++ es5.js
@@ -1,6 +1,6 @@
 module.exports = {
   'extends': [
-    'eslint-config-keeto/rules/es5'
+    'eslint-config-correct/rules/es5'
   ],
   'rules': {}
 };
--- es6.js
+++ es6.js
@@ -1,7 +1,7 @@
 module.exports = {
   'extends': [
-    'eslint-config-keeto/rules/es5',
-    'eslint-config-keeto/rules/es6'
+    'eslint-config-correct/rules/es5',
+    'eslint-config-correct/rules/es6'
   ],
   'rules': {}
 };
